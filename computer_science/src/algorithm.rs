b2874dde740eef578a25450485a0921ca0fb446c
Sarrnot <sarrnot@sarrnot.com>
2024-10-29 14:40:13 +0100

    add non-generic binary search in Rust

--- computer_science/src/algorithm.rs
+++ computer_science/src/algorithm.rs
@@ -1,1 +1,31 @@
-pub fn binary_search() {}
+pub fn binary_search(list: Vec<i32>, target: i32) -> Result<usize, ()> {
+    if list.len() == 0 {
+        return Err(());
+    }
+
+    let mut left: usize = 0;
+    let mut right: usize = list.len() - 1;
+
+    loop {
+        if right - left <= 1 {
+            if list[left] == target {
+                return Ok(left);
+            };
+            if list[right] == target {
+                return Ok(right);
+            }
+            return Err(());
+        }
+
+        let tested_position: usize = (left + right) / 2;
+        let tested_value: i32 = list[tested_position];
+
+        if target == tested_value {
+            return Ok(tested_position);
+        } else if target < tested_value {
+            right = tested_position - 1;
+        } else {
+            left = tested_position + 1;
+        }
+    }
+}
--- computer_science/src/main.rs
+++ computer_science/src/main.rs
@@ -1,3 +1,12 @@
+use algorithm::binary_search;
+
+mod algorithm;
+
 fn main() {
-    println!("Hello, world!");
+    let search_message = match binary_search(vec![1, 3, 5], 5) {
+        Ok(index) => &*format!("Searched value index: {index}"),
+        Err(_) => "Searched value not found",
+    };
+
+    println!("{}", search_message);
 }
