e6aa7a5a8f9ff13b84646e4e0bc3e7e845961ed3
Sarrnot <sarrnot@sarrnot.com>
2025-01-24 09:17:13 +0100

    refactor binary search, add unit tests

--- computer_science/src/algorithm/binary_search.rs
+++ computer_science/src/algorithm/binary_search.rs
@@ -1,11 +1,14 @@
-// TODO: make list generic
-// TODO: add unit test
-pub fn binary_search<T>(list: Vec<T>, target: T) -> Result<usize, ()>
+/// Search through a sorted Vector for a target value.
+///
+/// Returns Some(index) of target value, or None if not found.
+///
+/// `list` should ideally be a custom struct (e.g. SortedVec) which guarantees that list is sorted. Not implemented for demo purposes.
+pub fn binary_search<T>(list: &Vec<T>, target: T) -> Option<usize>
 where
     T: Ord,
 {
     if list.len() == 0 {
-        return Err(());
+        return None;
     }
 
     let mut left: usize = 0;
@@ -14,22 +17,51 @@ where
     loop {
         if right - left <= 1 {
             if list[left] == target {
-                return Ok(left);
+                return Some(left);
             };
             if list[right] == target {
-                return Ok(right);
+                return Some(right);
             }
-            return Err(());
+            return None;
         }
 
         let tested_position: usize = (left + right) / 2;
 
         if target == list[tested_position] {
-            return Ok(tested_position);
+            return Some(tested_position);
         } else if target < list[tested_position] {
             right = tested_position - 1;
         } else {
             left = tested_position + 1;
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn returns_correct_index() {
+        let list = vec![1, 3, 8, 10, 19, 27];
+
+        let index = binary_search(&list, 3);
+        assert_eq!(index, Some(1));
+    }
+
+    #[test]
+    fn returns_none() {
+        let list = vec![3, 5, 9];
+
+        let index = binary_search(&list, 4);
+        assert_eq!(index, None);
+    }
+
+    #[test]
+    fn handles_empty() {
+        let list = vec![];
+
+        let index = binary_search(&list, 2);
+        assert_eq!(index, None);
     }
 }
--- computer_science/src/main.rs
+++ computer_science/src/main.rs
@@ -1,13 +1,4 @@
-use algorithm::binary_search::binary_search;
-
 mod algorithm;
 mod data_structure;
 
-fn main() {
-    let search_message = match binary_search(vec![1, 3, 5], 5) {
-        Ok(index) => &*format!("Searched value index: {index}"),
-        Err(_) => "Searched value not found",
-    };
-
-    println!("{}", search_message);
-}
+fn main() {}
